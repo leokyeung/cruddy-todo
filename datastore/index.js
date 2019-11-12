21d4acbe87e23cf471418c3bbc355c7a169484a8
leokyeung <leoyeung99@gmail.com>
2019-11-11 17:20:25 -0800

    Update

--- datastore/index.js
+++ datastore/index.js
@@ -8,9 +8,16 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    let newPath = path.join(exports.dataDir, `${id}.txt`);
+    fs.writeFile(newPath, text, (err) => {
+      if (err) {
+        throw ('error');
+      } else {
+        callback(null, { id, text });
+      }
+    });
+  });
 };
 
 exports.readAll = (callback) => {
